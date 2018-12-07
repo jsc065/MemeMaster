3e286b9b18f50b5c0051bc297358e36842eb6603
jsc065 <jsc065@ucsd.edu>
2018-12-07 00:38:33 -0800

    Add files via upload

--- js/library.js
+++ js/library.js
@@ -1,24 +1,81 @@
 var itemList = [];
 
-function lib() {
+function updateLibrary() {
+  document.getElementById('library').innerHTML = '';
+  console.log("Updating Library");
   itemList = [];
+  var count = 0;
 
   var user = firebase.auth().currentUser;
   var dbRef = firebase.database().ref('users/' + user.uid + '/memes');
   var thisRef = dbRef.once('value').then((snapshot) =>{
     snapshot.forEach((child) => {
         itemList.push(child.val());
-        var node = document.createElement("p");                 // Create a <li> node
-        var textnode = document.createTextNode("TEST");         // Create a text node
-        node.appendChild(textnode);                              // Append the text to <li>
-        document.getElementById("emptylibrary").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+        var meme = document.createElement("div");
+        meme.id = 'meme' + count;
+        meme.className = 'meme';
+        document.getElementById("library").appendChild(meme);
+
+        var image = document.createElement("img");
+        image.className = 'image';
+        image.src = child.val();
+        document.getElementById("meme" + count).appendChild(image);
+
+        var brk = document.createElement("br");
+        document.getElementById("meme" + count).appendChild(brk);
+
+        var but1 = document.createElement("div");
+        but1.className = "botbutton";
+        var edit = document.createElement("button");
+        edit.type = "button";
+        edit.innerHTML = "Edit";
+        but1.appendChild(edit);
+        document.getElementById("meme" + count).appendChild(but1);
+
+        var but2 = document.createElement("div");
+        but2.className = "botbutton";
+        var del = document.createElement("button");
+        del.type = "button";
+        del.innerHTML = "Delete";
+        del.setAttribute("memeid", child.key);
+        del.addEventListener("click", deleteMeme);
+
+        but2.appendChild(del);
+        document.getElementById("meme" + count).appendChild(but2);
+
+        var but3 = document.createElement("div");
+        but3.className = "botbutton";
+        var share = document.createElement("button");
+        share.type = "button";
+        share.innerHTML = "Share";
+        but3.appendChild(share);
+        document.getElementById("meme" + count).appendChild(but3);
+
+        var dl = document.createElement("a");
+        dl.innerHTML = "Download";
+        dl.href = child.val();
+        dl.className = "dllink";
+        document.getElementById("meme" + count).appendChild(dl);
+
+        count += 1;
         return false;
     })
+    if (itemList.length > 0) {
+      document.getElementById("empty").style.display = "none";
+    }
     return itemList;
   });
 }
 
-document.getElementById('test').addEventListener('click', lib);
+function deleteMeme(e) {
+    var user = firebase.auth().currentUser;
+    var dbRef = firebase.database().ref('users/' + user.uid + '/memes');
+		e.stopPropagation();
+		var memeID = e.target.getAttribute("memeid");
+		dbRef.child(memeID).remove();
+    updateLibrary();
+}
 
 function createMeme() {
   document.getElementById('cuModal').style.display = 'block';
@@ -31,23 +88,46 @@ function createMeme() {
 
   document.getElementById('cuconfirm').addEventListener('click', function () {
     document.getElementById('cuModal').style.display = 'none';
-
     var file = document.getElementById('upload3').files[0];
-    console.log(file);
+    if (file) {
+      console.log(file);
 
-    var user = firebase.auth().currentUser;
-    var storageRef = firebase.storage().ref('user/' + user.uid);
-    console.log("TEST " + user);
-    var thisRef = storageRef.child(file.name);
-    thisRef.put(file).then(function(snapshot) {
-      return snapshot.ref.getDownloadURL();
-    })
-    .then(function(url) {
-      console.log(`Successfully uploaded file and got download link - ${url}`);
-      database.ref('users/' + user.uid + '/memes').push(url);
+      var user = firebase.auth().currentUser;
+      var storageRef = firebase.storage().ref('user/' + user.uid);
+      console.log("TEST " + user);
+      var thisRef = storageRef.child(file.name);
+      thisRef.put(file).then(function(snapshot) {
+        return snapshot.ref.getDownloadURL();
+      })
+      .then(function(url) {
+        console.log(`Successfully uploaded file and got download link - ${url}`);
+        database.ref('users/' + user.uid + '/memes').push(url);
+        console.log("Adding Your Meme");
+        return url;
+      })
+      .then(updateLibrary);
+    } else {
+      //Not sure how to read from local file for templates if user
+      //didn't upload anything
 
-      return url;
-    });
+      /*file = '../templates/' + document.getElementById('template').value + '.jpg';
+      console.log(file);
+
+      var user = firebase.auth().currentUser;
+      var storageRef = firebase.storage().ref('user/' + user.uid);
+      console.log("TEST " + user);
+      var thisRef = storageRef.child(document.getElementById('template').value + '.jpg');
+      thisRef.put(file).then(function(snapshot) {
+        return snapshot.ref.getDownloadURL();
+      })
+      .then(function(url) {
+        console.log(`Successfully uploaded file and got download link - ${url}`);
+        database.ref('users/' + user.uid + '/memes').push(url);
+
+        return url;
+      });*/
+    }
+    document.getElementById("upload3").value = "";
   });
   document.getElementById('cucancel').addEventListener('click', function () {
     document.getElementById('cuModal').style.display = 'none';
@@ -62,13 +142,15 @@ function initApp() {
       var photoURL = user.photoURL;
       var uid = user.uid;
       var providerData = user.providerData;
+
+      updateLibrary();
     } else {
       document.location.href = '../index.html';
     }
   });
-}
 
-document.getElementById('create').addEventListener('click', createMeme);
+  document.getElementById('create').addEventListener('click', createMeme);
+}
 
 window.onload = function() {
   initApp();
